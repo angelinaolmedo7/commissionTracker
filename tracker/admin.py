3b9e000b01a3becb88bdcb193eb3600c7fc8ab40
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-12-08 22:30:43 -0800

    Admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Commission, ArchivedCommission
 # Register your models here.
+
+admin.site.register(Commission)
+admin.site.register(ArchivedCommission)
