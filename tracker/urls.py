842379ee0d2365777db102e69d537bbfab0e9de9
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-12-10 13:26:28 -0800

    Commission and Archived Commission views

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:commission_id>/', views.detail, name='commission detail'),
+    path('archive/<int:archivedCommission_id>/', views.archived_detail, name='archived commission detail'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
+from .models import Commission, ArchivedCommission
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("tracker index")
+    active_commission_list = Commission.objects.order_by('creation_date')
+    output = ', '.join([c.commission_title for c in active_commission_list])
+    return HttpResponse(output)
+
+
+def detail(request, commission_id):
+    return HttpResponse("Commission %s." % commission_id)
+
+
+def archived_detail(request, archivedCommission_id):
+    return HttpResponse("Archived Commission %s." % archivedCommission_id)
