b3bc0fc7784fc34f1a2752ee9359d9c7cb6f5ccc
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-12-13 15:28:29 -0800

    Added feedback app
    https://github.com/ludrao/django-tellme

--- commissionTracker/urls.py
+++ commissionTracker/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('tracker/', include('tracker.urls')),
     path('admin/', admin.site.urls),
+    path('tellme/', include("tellme.urls")),
 ]
